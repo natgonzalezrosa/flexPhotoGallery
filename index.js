139ceb6d5759ed3647e2bfc49f59925633c97100
natgonzalezrosa <natgonzalezrosa@gmail.com>
2021-09-22 23:07:15 -0400

    changed layout to flexbox

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
+
 async function getPhotos() {
     let response = await fetch("photos.json")
     let photos = await response.json()
     return photos
 }
 
-function getPhotosHtml(photos) {
+function getMyPhotosHtml(photos) {
     let myPhotosHtml = photos.map(photo => {
         return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
     }).join('')
-    
-    return `<div class="my-photos">${myPhotosHtml}</div>`   
+    return `<div class="my-photos">${myPhotosHtml}</div>`
 }
 
-getPhotos().then(photos => {    
+getPhotos().then(photos => {
+    let myPhotosHtml = getMyPhotosHtml(photos)
     document.body.innerHTML = `<div class="my-gallery">
-        <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
-        ${getPhotosHtml(photos)}
-    </div>`    
+        <img id="my-selected-photo" class="my-photo"
+        src="https://picsum.photos/id/${photos[2].id}/300/300" />
+        ${myPhotosHtml}
+    </div>`
     
     let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
     myPhotoImgs.forEach(photoImg => {
-        photoImg.addEventListener("click", event => {
-            let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`
-
+        photoImg.addEventListener("click", e => {
+            let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `200/200`
             let selectedPhoto = document.getElementById("my-selected-photo")
             selectedPhoto.src = selectedPhotoSrc
             selectedPhoto.style.display = "inline"
         })
-    })
+    })    
 })
